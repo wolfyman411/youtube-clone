fe9b062da0aff02dbca9b66998fa01380c35a003
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-07-16 16:24:08 -0400

    fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Video from './pages/Video/Video'
+import Home from './Pages/Home/Home'
+import Video from './Pages/Video/Video'
 
 export default function App() {
 
